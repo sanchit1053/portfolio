0407da317f6b6d2cc35eb94faa3aa017f108e6da
sanchit1053 <sanchit1053@gmail.com>
2022-07-08 19:51:16 +0530

    navbar demo

---
+++ static/js/navigation.js
@@ -0,0 +1,14 @@
+const nav = document.querySelector(".navigation")
+const toggle = document.querySelector(".nav-toggle")
+console.log(nav)
+console.log(toggle)
+toggle.addEventListener("click", () => {
+    const visibility = nav.getAttribute("data-visible");
+    console
+    if(visibility === "false"){
+        nav.setAttribute("data-visible", "true");
+    }
+    else{
+        nav.setAttribute("data-visible", "false");
+    }
+})
